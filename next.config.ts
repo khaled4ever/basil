fa8d1383c29a7dfc1088847a83c04d8c48f4c669
Sarah <saracarfix@gmail.com>
2026-02-08 23:55:09 +0000

    هل استطيع اضافة .svg

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,9 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: 'https',
